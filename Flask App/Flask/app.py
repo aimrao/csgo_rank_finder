6b105e323f55d80d5ae8bc6ffdee3f994e12cbea
Pankaj Yadav <ypankaj651@gmail.com>
2021-11-16 01:43:47 +0530

    minor bug fix

--- Flask App/Flask/app.py
+++ Flask App/Flask/app.py
@@ -15,10 +15,6 @@ def index():
             try:
                 flash('Query submitted, please wait while we reveal the ranks...', 'info')
                 tb = find_rank_new(take_input(content))
-                tb_str = PrettyTable()
-                tb_str.field_names = ['Name', 'Rank', 'Best', 'Wins']
-                tb_str.add_rows(val[:4] for val in tb)
-                tb.append(tb_str)
                 tries = 0
                 if not content:
                     session.pop('_flashes', None)
@@ -27,6 +23,10 @@ def index():
                     session.pop('_flashes', None)
                     flash('Invalid console output.', 'danger')
                 else:
+                    tb_str = PrettyTable()
+                    tb_str.field_names = ['Name', 'Rank', 'Best', 'Wins']
+                    tb_str.add_rows(val[:4] for val in tb)
+                    tb.append(tb_str)
                     session.pop('_flashes', None)
                     flash('Hooray! Ranks revealed.', 'success')
                     return render_template('index.html', result=tb)
